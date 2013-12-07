cc01ac0800b573a8be79e1aeff5df47ca7deb2f3
beyamor <beyamor@gmail.com>
2013-12-07 13:48:28 -0800

    result for Seq

--- src/momo/core.clj
+++ src/momo/core.clj
@@ -1,3 +1,38 @@
 (ns momo.core)
 
-(declare <- return with)
+(declare ^:dynamic bind)
+(declare ^:dynamic return)
+
+(declare <-)
+
+(def monad-fs #{:bind :return})
+
+(defn keywordify-keys
+  [m]
+  (into {}
+        (for [[k v] m]
+          [(keyword k) v])))
+
+(defmacro defmonad
+  [m & {:as impl}]
+  (let [impl (-> impl
+               keywordify-keys
+               (select-keys monad-fs))]
+    `(def ~m ~impl)))
+
+(defn bindings
+  [m]
+  (->>
+    (for [f monad-fs]
+      `(~(symbol "momo.core" (name f)) (~m ~f)))
+    (apply concat)
+    vec))
+
+(defmacro with
+  [m & body]
+  `(binding ~(bindings m)
+     ~@body))
+
+(defmonad Seq
+          return (fn [v]
+                   [v]))
--- test/momo/core_test.clj
+++ test/momo/core_test.clj
@@ -1,15 +1,8 @@
 (ns momo.core-test
   (:use clojure.test)
   (:require [momo.core :as m
-             :refer [<- return]]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+             :refer [<- return Seq]]))
 
 (deftest seq-test
-         (is (= [[1 :a] [1 :b] [2 :a] [2 :b]]
-                (m/with seq
-                      (<- [x [1 2]
-                           y [:a :b]]
-                          (return [x y]))))))
+         (m/with Seq
+                 (is (= [1] (return 1)))))
